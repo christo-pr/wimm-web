a6b56db74a6d82f7b12d493ccb6689e883da70a2
Christofer Flores <cristofer.flort@gmail.com>
2020-02-11 12:18:26 -0600

    Change folder structure for a more simple one

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import Category from "./ui/Category"
+import Category from "./Category"
 import utils from "../utils"
 
 function ExpenseItem(props) {
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-export { default as Navbar } from "./ui/Navbar"
-export { default as BottomBar } from "./ui/BottomBar"
+export { default as Navbar } from "./Navbar"
+export { default as BottomBar } from "./BottomBar"
 
 export { default as DateFilter } from "./DateFilter"
 export { default as ExpenseItem } from "./ExpenseItem"
