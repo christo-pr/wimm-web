ed8fdbbbcde696fdd67a1a278fd9546dc0151df2
Christofer Flores <cristofer.flort@gmail.com>
2020-09-02 18:31:27 -0500

    Add paycheck item button

--- src/pages/PaycheckTrackerPage.js
+++ src/pages/PaycheckTrackerPage.js
@@ -4,20 +4,55 @@ import { PaycheckForm } from "../components"
 
 function PaycheckTrackerPage() {
   return (
-    <div className="uk-flex uk-flex-around uk-flex-middle uk-background-default">
-      <div className="paycheck-amount uk-text-center uk-padding-small">
-        <PaycheckForm
-          label="Total Mes:"
-          amount={150}
-          onChangeAmount={(amount) => {
-            console.log("new ahorro", amount)
-          }}
-        />
+    <>
+      <div className="uk-flex uk-flex-around uk-flex-middle uk-background-default">
+        <div className="paycheck-amount uk-text-center uk-padding-small">
+          <PaycheckForm
+            label="Total Mes:"
+            amount={150}
+            onChangeAmount={(amount) => {
+              console.log("new ahorro", amount)
+            }}
+          />
+        </div>
+        <div className="paycheck-amount uk-text-center uk-padding-small">
+          <PaycheckForm label="Total Ahorro:" amount={150} editable={false} />
+        </div>
       </div>
-      <div className="paycheck-amount uk-text-center uk-padding-small">
-        <PaycheckForm label="Total Ahorro:" amount={150} editable={false} />
+      <div data-uk-sticky className="paycheck-add-btn">
+        <button
+          type="button"
+          className="uk-button uk-button-success uk-width-expand"
+        >
+          Agregar +
+        </button>
       </div>
-    </div>
+      <hr className="uk-divider-small uk-text-center"></hr>
+      <ul
+        className="uk-list uk-list-divider"
+        data-uk-scrollspy="target: > li; cls: uk-animation-fade; delay: 100"
+      >
+        <li>testing</li>
+        <li>testing</li>
+        <li>testing</li>
+        <li>testing</li>
+        <li>testing</li>
+        <li>testing</li>
+        <li>testing</li>
+        <li>testing</li>
+        <li>testing</li>
+        <li>testing</li>
+        <li>testing</li>
+        <li>testing</li>
+        <li>testing</li>
+        <li>testing</li>
+        <li>testing</li>
+        <li>testing</li>
+        <li>testing</li>
+        <li>testing</li>
+        <li>testing</li>
+      </ul>
+    </>
   )
 }
 
